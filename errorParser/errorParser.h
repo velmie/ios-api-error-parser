5b4d1f5742d5ba1f03a198b0df2d66d0d8fde6e6
dmitrijmihajluk <mikhajlyuk@gmail.com>
2019-05-30 16:53:22 +0300

    Initial Commit

---
+++ errorParser/errorParser.h
@@ -0,0 +1,19 @@
+//
+//  errorParser.h
+//  errorParser
+//
+//  Created by Дмитрий Михайлюк on 5/30/19.
+//  Copyright © 2019 velmie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for errorParser.
+FOUNDATION_EXPORT double errorParserVersionNumber;
+
+//! Project version string for errorParser.
+FOUNDATION_EXPORT const unsigned char errorParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <errorParser/PublicHeader.h>
+
+
